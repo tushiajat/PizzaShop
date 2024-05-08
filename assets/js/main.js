b7a2708c8f50cb8a8cb0484a9c915d2d4fbdd5ef
unknown <tushijafrin@gmail.com>
2024-05-08 10:19:36 +0600

    comment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 // set navbar bg
-window.addEventListener("scroll", function () {
-  var navbar = document.getElementById("myNavbar");
-  if (window.scrollY > 0) {
-    navbar.classList.add("navbar-fixed");
-  } else {
-    navbar.classList.remove("navbar-fixed");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   var navbar = document.getElementById("myNavbar");
+//   if (window.scrollY > 0) {
+//     navbar.classList.add("navbar-fixed");
+//   } else {
+//     navbar.classList.remove("navbar-fixed");
+//   }
+// });
 
 // splide
 
